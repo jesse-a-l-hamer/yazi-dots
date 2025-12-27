47392b15b96461be4f6441bfa7b4c8ffb03a90cd
Jesse Hamer <jesse.a.l.hamer@gmail.com>
2025-12-27 12:39:28 -0600

    feat: initial commit

---
+++ init.lua
@@ -0,0 +1,76 @@
+require("full-border"):setup({
+	type = ui.Border.ROUNDED,
+})
+require("git"):setup()
+require("recycle-bin"):setup()
+require("restore"):setup()
+require("gvfs"):setup({
+	-- (Optional) Allowed keys to select device.
+	which_keys = "1234567890qwertyuiopasdfghjklzxcvbnm-=[]\\;',./!@#$%^&*()_+{}|:\"<>?",
+
+	-- (Optional) Save file.
+	-- Default: ~/.config/yazi/gvfs.private
+	save_path = os.getenv("HOME") .. "/.config/yazi/gvfs.private",
+
+	-- (Optional) Input box position.
+	-- Default: { "top-center", y = 3, w = 60 },
+	-- Position, which is a table:
+	-- 	`1`: Origin position, available values: "top-left", "top-center", "top-right",
+	-- 	     "bottom-left", "bottom-center", "bottom-right", "center", and "hovered".
+	--         "hovered" is the position of hovered file/folder
+	-- 	`x`: X offset from the origin position.
+	-- 	`y`: Y offset from the origin position.
+	-- 	`w`: Width of the input.
+	-- 	`h`: Height of the input.
+
+	-- (Optional) Select where to save passwords.
+	-- Default: nil
+	-- Available options: "keyring", "pass", or nil
+	-- password_vault = "keyring",
+
+	-- (Optional) Only need if you set password_vault = "pass"
+	-- Read the guide at SECURE_SAVED_PASSWORD.md to get your key_grip
+	-- key_grip = "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+
+	-- (Optional) Auto-save password after mount.
+	-- Default: false
+	-- save_password_autoconfirm = true,
+	-- (Optional) mountpoint of gvfs. Default: /run/user/USER_ID/gvfs
+	-- On some system it could be ~/.gvfs
+	-- You can't decide this path, it will be created automatically. Only changed if you know where gvfs mountpoint is.
+	-- Use command `ps aux | grep gvfs` to search for gvfs process and get the mountpoint path.
+	-- root_mountpoint = (os.getenv("XDG_RUNTIME_DIR") or ("/run/user/" .. ya.uid())) .. "/gvfs"
+})
+require("kdeconnect-send"):setup({
+	auto_select_single = false,
+})
+
+Status:children_add(function(self)
+	local h = self._current.hovered
+	if h and h.link_to then
+		return " -> " .. tostring(h.link_to)
+	else
+		return ""
+	end
+end, 3300, Status.LEFT)
+
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if not h or ya.target_family() ~= "unix" then
+		return ""
+	end
+
+	return ui.Line({
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		":",
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		" ",
+	})
+end, 500, Status.RIGHT)
+
+Header:children_add(function()
+	if ya.target_family() ~= "unix" then
+		return ""
+	end
+	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+end, 500, Header.LEFT)
